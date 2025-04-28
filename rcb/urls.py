014fd335ec6cea1f9e46070785b1d151eab01aa4
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-04-28 06:10:11 +0530

    Delete rcb directory

--- rcb/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rcb.views import *
-app_name="anything"
-urlpatterns = [path("captain/",captain,name="captain"),
-               path("vicecaptain/",vicecaptain,name="vicecaptain"),
-    
-]
-
