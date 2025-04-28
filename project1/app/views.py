dee95f1b2af6b6f550d2b807f3e3de3211641686
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-04-28 06:08:08 +0530

    Add files via upload

---
+++ project1/app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Jayanth(request):
+    return HttpResponse('<h1 style="color:pink background-color:black box-shadow:0px 0px 5px"><marquee><i>90Cr-1 God is Great</i></marquee></h1>')
