5851c6daba23231ddea10db5727b03f65a473e46
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-04-28 06:10:02 +0530

    Delete mi directory

--- mi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from mi.views import *
-app_name="Everything"
-urlpatterns = [path("captain/",captain,name="captain"),
-               path("vicecaptain/",vicecaptain,name="vicecaptain"),
-    
-]
-
