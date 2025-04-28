2dbeac90e5552c5fba9c200340c93e51086203ea
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-04-28 06:09:36 +0530

    Delete csk directory

--- csk/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from csk.views import *
-app_name="something"
-urlpatterns = [path("captain/",captain,name="captain"),
-               path("vicecaptain/",vicecaptain,name="vicecaptain"),
-    
-]
-
--- csk/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def captain(request):
-    return HttpResponse('<h1>RutuRaj Gaikward is the Captain')
-def vicecaptain(request):
-    return HttpResponse('<h1>Rachin Ravindra is vice captain</h1>')
