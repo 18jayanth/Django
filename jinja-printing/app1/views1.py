eb5d885a281d310fcac6d9fd9b94aa9e95cd10fe
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-04-28 10:44:03 +0530

    Update views1.py

--- jinja-printing/app1/views1.py
+++ jinja-printing/app1/views1.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index1(request):
-    return render(request,'index1.html')
+    d={'Roll':18,'branch':'cse'}
+    return render(request,'index1.html',context=d)
